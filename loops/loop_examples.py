dd0d8cf2455f71f748b66214b8b320b77e15b0e2
Sithum Geeth <115262911+sithum-devops@users.noreply.github.com>
2025-12-06 11:55:56 +0530

    Add examples of for and while loops
    Includes examples of both for and while loops demonstrating basic iteration.

--- loops/loop_examples.py
+++ loops/loop_examples.py
@@ -1,1 +1,14 @@
+# Basic loop examples
 
+# For loop
+for i in range(5):
+    print("For loop number:", i)
+
+for i in range(2,10):#it begins with 2
+    print("For loop number:", i)
+  
+# While loop
+num = 3
+while num > 0:
+    print("While loop:", num)
+    num -= 1
